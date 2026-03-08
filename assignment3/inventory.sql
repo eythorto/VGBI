64f274b4282f4cecc8840c5fde5c9bfa2be46e62
TelmaOskThorhallsdottir <thorhallsdottir01@gmail.com>
2026-03-08 17:44:39 +0000

    inventory

--- assignment3/inventory.sql
+++ assignment3/inventory.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS staging.dimInventory_v1 (
+CREATE TABLE IF NOT EXISTS staging.factInventory_v1 (
     id bigserial NOT NULL,
     store_id int NOT NULL,
     product_id int NOT NULL,
@@ -8,68 +8,53 @@ CREATE TABLE IF NOT EXISTS staging.dimInventory_v1 (
     rowCreated timestamptz NOT NULL DEFAULT timezone('utc', now())
 );
 
-CREATE INDEX ix_dimInventory_store_product ON public.dimInventory USING btree (store_id, product_id);
-
-CREATE TABLE IF NOT EXISTS public.dimInventory (
+CREATE TABLE IF NOT EXISTS public.factInventory (
     id bigserial NOT NULL,
     store_id int NOT NULL,
     product_id int NOT NULL,
+    snapshot_date date NOT NULL,
     stock_on_hand int NOT NULL,
 
     rowBatchId integer NOT NULL,
     rowCreated timestamptz NOT NULL DEFAULT timezone('utc', now()),
-    rowModified timestamptz NOT NULL DEFAULT timezone('utc', now()),
 
-    CONSTRAINT dimInventory_pkey PRIMARY KEY (id),
-    CONSTRAINT uq_dimInventory_store_product UNIQUE (store_id, product_id)
+    CONSTRAINT factInventory_pkey PRIMARY KEY (id)
 );
 
-CREATE OR REPLACE PROCEDURE staging.sp_dimInventory_v1_publish (IN p_batchid integer)
+CREATE INDEX IF NOT EXISTS ix_factInventory_store_product_date ON public.factInventory USING btree (store_id, product_id, snapshot_date);
+
+CREATE OR REPLACE PROCEDURE staging.sp_factInventory_v1_publish (IN p_batchid integer)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-
-    WITH cte_data AS
-    (
-        SELECT *
-        FROM staging.dimInventory_v1
-        WHERE rowBatchId = p_batchid
+    /*
+    eitt stock_on_hand er neikvætt
+    nokkur store_id eru ekki til. setja id sem -1 "Unknown Store"
+    */
+    INSERT INTO public.factInventory (
+        store_id,
+        product_id,
+        stock_on_hand,
+        snapshot_date,
+        rowBatchId
     )
-
-    MERGE INTO public.dimInventory trg
-    USING cte_data src
-    ON trg.store_id = src.store_id AND trg.product_id = src.product_id
-
-    WHEN MATCHED THEN
-        UPDATE SET
-            stock_on_hand = src.stock_on_hand,
-            rowModified   = timezone('utc', now()),
-            rowBatchId   = p_batchid
-
-    WHEN NOT MATCHED THEN
-        INSERT (
-            id,
-            store_id,
-            product_id,
-            stock_on_hand,
-            rowBatchId
-        )
-        VALUES (
-            src.id,
-            src.store_id,
-            src.product_id,
-            src.stock_on_hand,
-            p_batchid
-        );
+    SELECT 
+        store_id,
+        product_id,
+        stock_on_hand,
+        snapshot_date,
+        rowBatchId
+    FROM staging.factInventory_v1
+    WHERE rowBatchId = p_batchid;
 END;
 $$;
 
-CREATE OR REPLACE PROCEDURE staging.sp_dimInventory_v1_postprocess (IN p_batchid integer)
+CREATE OR REPLACE PROCEDURE staging.sp_factInventory_v1_postprocess (IN p_batchid integer)
 LANGUAGE plpgsql
 AS $$
 BEGIN
     DELETE
-    FROM staging.dimInventory_v1
+    FROM staging.factInventory_v1
     WHERE rowBatchId = p_batchid;
 END;
 
